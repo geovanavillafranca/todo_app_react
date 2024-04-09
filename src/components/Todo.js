b1733b6730a8fe4e0b00552845c9c480f8ad2a21
geovanavillafranca <geovanavillafranca@hotmail.com>
2024-04-09 09:49:39 -0300

    add style to <p> / task name, if checked will change the style of the text

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,7 +10,9 @@ const Todo = ({ todos, deleteTodo, updateStatus }) => {
             return (
                 <div className='todo-list'>
                   <input type='checkbox' className='todo-checkbox' checked={task.isCompleted} onClick={() => updateStatus(task.id, task.isCompleted)}></input>
-                  <p style={{textDecoration : task.isCompleted ? 'line-through' : "none", color: task.isCompleted ? "grey" : "white"}}>{task.name}</p>
+                  <p style={{textDecoration : task.isCompleted ? 'line-through' : "none", color: task.isCompleted ? "rgba(235, 238, 243, 0.671)" : "white", fontStyle: task.isCompleted ? 'italic' : 'normal'}}>
+                    {task.name}
+                  </p>
                   <div className='todo-icons'> 
                     {/* <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} /> */}
                     <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
